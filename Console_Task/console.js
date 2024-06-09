8e33eb06ea8f3235ce041d1464219f28f843c73a
Meet <meetsarvaiya05@gmail.com>
2024-06-09 17:02:46 +0530

    console task

---
+++ Console_Task/console.js
@@ -0,0 +1,213 @@
+// 1. **Print Hello World**
+console.log("Hello World");
+// 2. **Print a Number**
+console.log(1,2,3,4,5);
+// 3. **Print a Variable Value**
+var v = "Hello";
+console.log(v);
+// 4. **Print the Sum of Two Numbers**
+var num1 = 5, num2 = 10;
+console.log(num1 + num2);
+// 5. **Print a Concatenated String**
+var string1 = "Hello", string2 = "World";
+console.log(string1 + " " + string2);
+// 6. **Print the Length of a String**
+var str = "JavaScript";
+console.log(str.length);
+// 7. **Print an Array**
+var array = [1, 2, 3];
+console.log(array);
+// 8. **Print the First Element of an Array**
+console.log(array[0]);
+// 9. **Print a Boolean Value**
+var bool = true;
+console.log(bool);
+// 10. **Print an Object**
+var obj = { name: "Meet", age: 21 };
+console.log(obj);
+// 11. **Print the Value of an Object Property**
+var obj = { name: "Meet", age: 21 };
+console.log(obj.name);
+// 12. **Print a Mathematical Expression**
+console.log(2 * (5 + 10));
+// 13. **Print a Function Call Result**
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(5, 10));
+// 14. **Print a Random Number**
+console.log(Math.random());
+// 15. **Print the Current Date and Time**
+
+// 16. **Print the Type of a Variable**
+var v = "Hello";
+console.log(typeof v);
+// 17. **Print a Null Value**
+var nullValue = null;
+console.log(nullValue);
+// 18. **Print Undefined Value**
+var undefinedValue;
+console.log(undefinedValue);
+// 19. **Print a String in Uppercase**
+var lowercaseStr = "hello";
+console.log(lowercaseStr.toUpperCase());
+// 20. **Print a String in Lowercase**
+var uppercaseStr = "HELLO";
+console.log(uppercaseStr.toLowerCase());
+// //////////////////////////////
+
+// 1. **Print your favorite quote**
+console.log("May you live all the days of your life.");
+// 2. **Print the result of subtracting two numbers**
+var num1 = 10;
+var num2 = 5;
+console.log(num1 - num2);
+// 3. **Print a greeting message using a variable**
+var n = "Meet";
+console.log("Hello, " + n + "!");
+// 4. **Print the result of multiplying two numbers**
+console.log(5 * 10);
+// 5. **Print a string with both single and double quotes**
+console.log(`String with single quotes: 'Hello'`);
+console.log("String with double quotes: \"World\"");
+// 6. **Print the character at a specific index in a string**
+var str = "Hello";
+console.log(str.charAt(2));
+// 7. **Print the last element of an array**
+var array = ["apple", "banana", "orange"];
+console.log(array[array.length - 1]);
+// 8. **Print the length of an array**
+var array = ["apple", "banana", "orange"];
+console.log(array.length);
+// 9. **Print the result of a comparison operation (greater than)**
+console.log(5 > 3);
+// 10. **Print an array of numbers**
+console.log([1, 2, 3, 4, 5]);
+// 11. **Print the sum of the first and last elements in an array**
+var numbers = [1, 2, 3, 4, 5];
+console.log(numbers[0] + numbers[numbers.length - 1]);
+// 12. **Print the result of dividing two numbers**
+console.log(10 / 2);
+// 13. **Print a number to a fixed decimal place**
+console.log(3.14159.toFixed(2));
+// 14. **Print the minimum value from a set of numbers**
+console.log(Math.min(10, 20, 30, 40, 50));
+// 15. **Print the maximum value from a set of numbers**
+console.log(Math.max(10, 20, 30, 40, 50));
+// 16. **Print a boolean expression (using AND operator)**
+console.log(true && false);
+// 17. **Print a boolean expression (using OR operator)**
+console.log(true || false);
+// 18. **Print the result of concatenating an array and a string**
+console.log(["apple", "banana"].concat("orange"));
+// 19. **Print the square of a number**
+console.log(Math.pow(5, 2));
+// 20. **Print the cube of a number**
+console.log(Math.pow(5, 3));
+// //////////////////////
+
+// Sure, here is another set of 20 practice problems for `console.log()`:
+
+// 1. **Print your favorite quote**
+console.log("The only way to do great work is to love what you do.");
+// 2. **Print the result of multiplying two numbers**
+console.log(5 * 7);
+// 3. **Print a string that includes a newline character**
+console.log("Hello\nWorld");
+// 4. **Print an array of your top 3 favorite movies**
+console.log(["Inception", "The Shawshank Redemption", "The Dark Knight"]);
+// 5. **Print the second element in an array**
+console.log(["apple", "banana", "orange"][1]);
+// 6. **Print the result of a complex mathematical expression (e.g., (8 + 2) * (5 / 2))**
+console.log((8 + 2) * (5 / 2));
+// 7. **Print the type of a number**
+console.log(typeof 10);
+// 8. **Print the result of comparing two numbers (e.g., 5 > 3)**
+console.log(5 > 3);
+// 9. **Print a greeting message that includes a variable (e.g., "Hello, " + name)**
+var n = "Meet";
+console.log("Hello, " + n);
+// 10. **Print the length of an array**
+var arr = [1, 2, 3];
+console.log(arr.length);
+// 11. **Print the result of converting a string to a number**
+console.log(parseInt("42"));
+// 12. **Print a boolean value that represents whether a string includes a specific substring**
+console.log("Hello, world!".includes("world"));
+// 13. **Print the result of a ternary operation**
+console.log(5 > 3 ? "Yes" : "No");
+// 14. **Print the properties of an object on separate lines**
+var obj = {a: 1, b: 2, c: 3};
+for (var key in obj) {
+  console.log(key + ": " + obj[key]);
+}
+// 15. **Print a formatted date (e.g., "2024-06-08")**
+
+// 16. **Print the character at a specific index in a string**
+console.log("Meet"[1]);
+// 17. **Print the result of adding an element to an array**
+var arr = [1, 2, 3];
+arr.push(4);
+console.log(arr);
+// 18. **Print the keys of an object**
+console.log(Object.keys({a: 1, b: 2, c: 3}));
+// 19. **Print the square of a number using Math.pow()**
+console.log(Math.pow(5, 2));
+// 20. **Print a reversed string**
+console.log("hello".split('').reverse().join(''));
+// ////////////////////////////////////
+
+// 1. **Print "Good Morning"**
+console.log("Good Morning");
+// 2. **Print the result of 7 multiplied by 6**
+console.log(7 * 6);
+// 3. **Print the value of a variable containing your age**
+let age = 25;
+console.log(age);
+// 4. **Print the result of subtracting 15 from 30**
+console.log(30 - 15);
+// 5. **Print the concatenation of "Good" and "Evening"**
+console.log("Good" + "Evening");
+// 6. **Print the length of the string "JavaScript is fun!"**
+console.log("JavaScript is fun!".length);
+// 7. **Print an array of three different car brands**
+console.log(["Toyota", "Honda", "Ford"]);
+// 8. **Print the last element of an array**
+var carBrands = ["Toyota", "Honda", "Ford"];
+console.log(carBrands[carBrands.length - 1]);
+// 9. **Print the boolean value of an expression (e.g., 10 > 5)**
+console.log(10 > 5);
+// 10. **Print an object representing a book with properties: title, author, and year**
+var book = {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    year: 1960
+};
+console.log(book);
+// 11. **Print the value of the "author" property from the book object**
+var book = {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    year: 1960
+};
+console.log(book.author);
+// 12. **Print the result of dividing 20 by 4**
+console.log(20 / 4);
+// 13. **Print the result of a function that returns the square of 5**
+function square(num) {
+    return num * num;
+}
+console.log(square(5));
+// 14. **Print a random integer between 1 and 50**
+
+// 15. **Print the current year**
+
+// 16. **Print the type of a number**
+console.log(typeof 5);
+// 17. **Print a null value explicitly**
+console.log(null);
+// 18. **Print an undefined value explicitly**
+console.log(undefined);
+// 19. **Print a string in reverse order**
+console.log("hello".split('').reverse().join(''));
+// 20. **Print a string with all vowels removed**
